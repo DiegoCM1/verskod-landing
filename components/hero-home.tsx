89bc5e1456fc256bfac32932c7091ccc3aba18e7
Diego Colín <luiscolin764@gmail.com>
2025-06-28 15:32:09 -0600

    Modified title for a better look on phone and pc

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -11,17 +11,17 @@ export default function HeroHome() {
     <section>
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         {/* Hero content */}
-        <div className="py-12 md:py-20">
+        <div className="py-8 md:py-12">
           {/* Section header */}
           <div className="pb-12 text-center md:pb-20">
             <h1
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-3xl/snug font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
               Desarrollo de software y capacitación personalizada de IA para tu
               empresa.
             </h1>
-            <div className="mx-auto max-w-3xl">
+            <div className="mx-auto max-w-2xl">
               <p
                 className="mb-8 text-xl text-indigo-200/65"
                 data-aos="fade-up"
